f3ba6181f828c0204ad0f1ae9b88eb96ecab89e0
Francis <francis.bouchard91@gmail.com>
2021-12-02 15:27:56 -0500

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val input = readInput("input_day1")
+    var count = 0
+    input.forEachIndexed { index, element ->
+        if(index > 0) {
+            if((input[index-1].toInt()) < (element.toInt())) {
+                count++
+                }
+        }
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    print(count)
 }
